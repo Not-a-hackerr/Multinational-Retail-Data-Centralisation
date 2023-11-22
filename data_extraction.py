ed2de6fab3534710efd0d4d94dfa383177fa9a95
Nath <nmwasuku@yahoo.co.uk>
2023-11-22 09:50:27 +0000

    Adds Extraction of card payment details from pdf sources using tabula

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,18 @@
 import tabula
+import pandas as pd
 
 class DataExtractor:
     def read_rds_engine(self):
         pass
     
+    def retrieve_pdf_data(self):
+        pdf_path = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+        dfs = tabula.read_pdf(pdf_path, stream=True, pages="1,2,3,4,5,6")
+        dfs = pd.concat(dfs)
+        return dfs
+
+
+# DE = DataExtractor()
+# card_payment_data = DE.retrieve_pdf_data()
+# card_payment_data
 
