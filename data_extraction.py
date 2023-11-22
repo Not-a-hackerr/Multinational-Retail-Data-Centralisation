9433a9ea58c3e1486861ee19026a886c8ac7f7cd
Nath <nmwasuku@yahoo.co.uk>
2023-11-22 21:29:36 +0000

    Adds sqlalchemy engine connection aswell as reading the users data from the cloud and converting it into a dataframe

--- data_extraction.py
+++ data_extraction.py
@@ -1,15 +1,31 @@
+import database_utils
 import tabula
 import pandas as pd
+import inspect
+from sqlalchemy import text 
 from dateutil.parser import parse
 
 class DataExtractor:
-    def read_rds_engine(self):
-        pass
-    
+    """
+
+    sqlalchemy helps you connect to cloud service, here we are connecting to a aws cloud to read data
+    to then clean it and add it to pgadmin
+
+    Engine is essentially the connection between this local device and the cloud 
+
+    parameter - table_name is a parameter because there are a wind variety of object that table_name could be
+    variable - The exact object that is put in for table_name 
+    """
+    def read_rds_engine(self, table_name, engine):
+        with engine as connection:
+            users = pd.read_sql_table(f"{table_name}", engine)
+        return users
+        
     def retrieve_pdf_data(self):
         pdf_path = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
-        dfs = tabula.read_pdf(pdf_path, stream=True, pages="all")
-        dfs = pd.concat(dfs)
-        return dfs
+        payment = tabula.read_pdf(pdf_path, stream=True, pages="all")
+        payment = pd.concat(dfs)
+        return payment
+
 
 
--- database_utils.py
+++ database_utils.py
@@ -1,19 +1,36 @@
 import yaml
+import psycopg2
+from sqlalchemy import create_engine
+
+from sqlalchemy import inspect
+
 
 class DatabaseConnector:
-    
+
     def read_db_creds(self):
-        pass
+        with open('db_creds.yaml') as f:
+            data_base_creds = yaml.load(f, Loader=yaml.FullLoader)
+        return data_base_creds
+        
     
-    # Will read credentials from read_db_creds
+    # Reads credentials from read_db_creds dictionay
     def init_db_engine(self):
-        pass
+        db_creds_dict = self.read_db_creds()
+        engine = create_engine(f"postgresql://{db_creds_dict['RDS_USER']}:{db_creds_dict['RDS_PASSWORD']}@{db_creds_dict['RDS_HOST']}:{db_creds_dict['RDS_PORT']}/{db_creds_dict['RDS_DATABASE']}")
+        engine = engine.connect()
+        return engine
 
     def list_db_tables(self):
-        pass
-    
+        engine = self.init_db_engine()
+        inspector = inspect(engine)
+        table_names = inspector.get_table_names()
+        return table_names
+
     # Upload data in sales_data db in a table named dim_users
     def upload_to_db(self):
         pass
 
 
+DBC = DatabaseConnector()
+db_dict = DBC.read_db_creds()
+print(DBC.list_db_tables())
